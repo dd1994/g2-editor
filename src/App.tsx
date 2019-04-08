f81245daa9cc3eee1d3a5f4d2aeb7499a15921c4
jiangdi <jiangdi@weidian.com>
2019-04-08 17:24:57 +0800

    constructor

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,30 @@ import { data } from './components/viz/data/gdp';
 import G2 from '@antv/g2';
 const DataSet = require('@antv/data-set');
 
-const dataset = new DataSet.View()
-    .source(data, {
-        type: 'csv'
-    })
-    .transform({
-        type: 'filter',
-        callback(row: any) {
-            // 判断某一行是否保留，默认返回true
-            return ['China', 'Japan', 'United States'].includes(row['Entity']);
-        }
-    })
-    .transform({
-        type: 'map',
-        callback(row: any) {
-            row.GDP = Number(row.GDP);
-            return row;
-        }
-    });
-
 class App extends Component {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            data: new DataSet.View()
+                .source(data, {
+                    type: 'csv'
+                })
+                .transform({
+                    type: 'filter',
+                    callback(row: any) {
+                        // 判断某一行是否保留，默认返回true
+                        return ['China', 'Japan', 'United States'].includes(row['Entity']);
+                    }
+                })
+                .transform({
+                    type: 'map',
+                    callback(row: any) {
+                        row.GDP = Number(row.GDP);
+                        return row;
+                    }
+                })
+        };
+    }
     render() {
         const dimensions = ['xx', 'yy'];
         const measures = ['oo', 'mm'];
