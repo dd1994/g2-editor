78e53a8d7424d9885a195d334d154636979ac8a7
jiangdi <jiangdi@weidian.com>
2019-04-08 17:20:26 +0800

    add dataset

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,29 @@ import Button from 'antd/lib/button';
 
 import './App.css';
 import Data from './components/viz/data/data';
+import { data } from './components/viz/data/gdp';
+import G2 from '@antv/g2';
+const DataSet = require('@antv/data-set');
+
+const dataset = new DataSet.View()
+    .source(data, {
+        type: 'csv'
+    })
+    .transform({
+        type: 'filter',
+        callback(row: any) {
+            // 判断某一行是否保留，默认返回true
+            return ['China', 'Japan', 'United States'].includes(row['Entity']);
+        }
+    })
+    .transform({
+        type: 'map',
+        callback(row: any) {
+            row.GDP = Number(row.GDP);
+            return row;
+        }
+    });
+
 class App extends Component {
     render() {
         const dimensions = ['xx', 'yy'];
