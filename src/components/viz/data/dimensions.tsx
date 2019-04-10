b15243a16ffd02e3b661cecbf5dac88add2a3312
jiangdi <jiangdi@weidian.com>
2019-04-10 16:59:26 +0800

    fix css

--- src/components/viz/data/dimensions.tsx
+++ src/components/viz/data/dimensions.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Tag } from 'antd';
+import { Tag, Button } from 'antd';
 
-export default class Dimensions extends React.Component<{ dimensions: Array<string>; setDragItem: any }, {}> {
+export default class Dimensions extends React.Component<
+    { dimensions: Array<string>; setDragItem: any },
+    {}
+> {
     handleDragStart = (item: string, e: any) => {
         this.props.setDragItem(item);
     };
     render() {
         const dimensionItems = this.props.dimensions.map((item: string) => {
             return (
-                <li draggable key={item} onDragStart={this.handleDragStart.bind(this, item)}>
-                    <Tag color="blue">{item}</Tag>
+                <li
+                    draggable
+                    className="field-item"
+                    key={item}
+                    onDragStart={this.handleDragStart.bind(this, item)}
+                >
+                    <Button type="primary" shape="round" size="small" block>
+                        {item}
+                    </Button>
                 </li>
             );
         });
